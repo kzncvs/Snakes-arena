581f6df6bbfc21a044f24a9096586a8e25139d94
kazancevs <s@kazancev.xyz>
2018-05-13 00:47:39 +0300

    reimagined change_fight_info function

--- db_tools.py
+++ db_tools.py
@@ -27,47 +27,47 @@ def is_last_fight_waiting():
     return is_fight_waiting(get_fight_count())
 
 
-def change_fight_info(snake1_head=None, snake1_body=None, snake1_tail=None, snake2_head=None, snake2_body=None,
-                      snake2_tail=None, steps_left=None, is1bited=None, is2bited=None, snake2_id=None, snake1_step=None,
-                      snake2_step=None):
+def change_fight_info(fight_id, snake1_head=None, snake1_body=None, snake1_tail=None, snake2_head=None,
+                      snake2_body=None, snake2_tail=None, steps_left=None, is1bited=None, is2bited=None,
+                      snake2_id=None, snake1_step=None, snake2_step=None):
     db = sqlite3.connect(db_link)
     cursor = db.cursor()
     if snake1_head is not None:
         cursor.execute('UPDATE [fights] SET [snake1_head] = :snake1_head WHERE fight_id = :count',
-                       {'snake1_head': json.dumps(snake1_head), 'count': get_fight_count()})
+                       {'snake1_head': json.dumps(snake1_head), 'count': fight_id})
     if snake1_body is not None:
         cursor.execute('UPDATE [fights] SET [snake1_body] = :snake1_body WHERE fight_id = :count',
-                       {'snake1_body': json.dumps(snake1_body), 'count': get_fight_count()})
+                       {'snake1_body': json.dumps(snake1_body), 'count': fight_id})
     if snake1_tail is not None:
         cursor.execute('UPDATE [fights] SET [snake1_tail] = :snake1_tail WHERE fight_id = :count',
-                       {'snake1_tail': json.dumps(snake1_tail), 'count': get_fight_count()})
+                       {'snake1_tail': json.dumps(snake1_tail), 'count': fight_id})
     if snake2_head is not None:
         cursor.execute('UPDATE [fights] SET [snake2_head] = :snake2_head WHERE fight_id = :count',
-                       {'snake2_head': json.dumps(snake2_head), 'count': get_fight_count()})
+                       {'snake2_head': json.dumps(snake2_head), 'count': fight_id})
     if snake2_body is not None:
         cursor.execute('UPDATE [fights] SET [snake2_body] = :snake2_body WHERE fight_id = :count',
-                       {'snake2_body': json.dumps(snake2_body), 'count': get_fight_count()})
+                       {'snake2_body': json.dumps(snake2_body), 'count': fight_id})
     if snake2_tail is not None:
         cursor.execute('UPDATE [fights] SET [snake2_tail] = :snake2_tail WHERE fight_id = :count',
-                       {'snake2_tail': json.dumps(snake2_tail), 'count': get_fight_count()})
+                       {'snake2_tail': json.dumps(snake2_tail), 'count': fight_id})
     if is2bited is not None:
         cursor.execute('UPDATE [fights] SET [snake2_bited] = :isbited WHERE fight_id = :count',
-                       {'count': get_fight_count(), 'isbited': int(is2bited)})
+                       {'count': fight_id, 'isbited': int(is2bited)})
     if is1bited is not None:
         cursor.execute('UPDATE [fights] SET [snake1_bited] = :isbited WHERE fight_id = :count',
-                       {'count': get_fight_count(), 'isbited': int(is1bited)})
+                       {'count': fight_id, 'isbited': int(is1bited)})
     if steps_left is not None:
         cursor.execute('UPDATE [fights] SET [steps_left] = :steps_left WHERE fight_id = :count',
-                       {'steps_left': steps_left, 'count': get_fight_count()})
+                       {'steps_left': steps_left, 'count': fight_id})
     if snake2_id is not None:
         cursor.execute('UPDATE [fights] SET [snake2] = :snake WHERE fight_id = :count',
-                       {'snake': snake2_id, 'count': get_fight_count()})
+                       {'snake': snake2_id, 'count': fight_id})
     if snake1_step is not None:
         cursor.execute('UPDATE [fights] SET [snake1_step] = :snake WHERE fight_id = :count',
-                       {'snake': snake1_step, 'count': get_fight_count()})
+                       {'snake': snake1_step, 'count': fight_id})
     if snake2_step is not None:
         cursor.execute('UPDATE [fights] SET [snake2_step] = :snake WHERE fight_id = :count',
-                       {'snake': snake2_step, 'count': get_fight_count()})
+                       {'snake': snake2_step, 'count': fight_id})
     db.commit()
     db.close()
 
@@ -95,4 +95,3 @@ def get_fight_info(fight_id):
 def is_steps_completed(fight_id):
     heh = get_fight_info(fight_id)
     return heh[12] != 0 and heh[13] != 0
-
