d39ad0e709ed00a4454b7b939302c8e571894845
kazancevs <s@kazancev.xyz>
2018-05-12 23:49:15 +0300

    added function for returning current battle info

--- arena.py
+++ arena.py
@@ -1,3 +1,6 @@
+STEPS_LIMIT = 30
+
+from flask import *
 import db_tools
 import uuid
 
@@ -13,3 +16,33 @@ def battle_init():
         battle_id = db_tools.fight_init(snake_id)
         return {'battle_id': battle_id, 'snake_id': snake_id}
 
+
+def battle_tick(snake_id, battle_id):
+    if db_tools.is_fight_waiting(battle_id) is True:
+        return jsonify({}), 202
+    else:
+        battle_info = db_tools.get_fight_info(battle_id)
+        snake1 = {
+            'head': json.loads(battle_info[4]),
+            'body': json.loads(battle_info[5]),
+            'tail': json.loads(battle_info[6]),
+            'is_bited': bool(battle_info[7])
+        }
+        snake2 = {
+            'head': json.loads(battle_info[8]),
+            'body': json.loads(battle_info[9]),
+            'tail': json.loads(battle_info[10]),
+            'is_bited': bool(battle_info[11])
+        }
+        response = {}
+        if battle_info[1] == snake_id:
+            response = {
+                'snakes': {'ally': snake1, 'enemy': snake2},
+                'battle': {'steps_left': battle_info[3], 'battle_id': battle_id, 'snake_id': snake_id}
+            }
+        elif battle_info[2] == snake_id:
+            response = {
+                'snakes': {'ally': snake2, 'enemy': snake1},
+                'battle': {'steps_left': battle_info[3], 'battle_id': battle_id, 'snake_id': snake_id}
+            }
+        return jsonify(response), 200
